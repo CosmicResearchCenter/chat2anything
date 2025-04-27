b4bd01716e411b1551cbd90c9a8b0dd4ff2a1ccc
markyangkp <markyangkp@outlook.com>
2025-04-27 21:42:20 +0800

    移除不必要的嵌入相关配置项，简化设置类

--- backend/config/config_info.py
+++ backend/config/config_info.py
@@ -24,13 +24,6 @@ class Settings(BaseSettings):
     MYSQL_USER:str
     MYSQL_PASSWORD:str
 
-    EMBEDDING_BASE_URL:str
-    EMBEDDING_API_KEY:str
-    EMBEDDING_MODEL_PROVIDER :str 
-    EMBEDDING_MODEL :str
-    
-    LLM_PROVIDER :str
-    # 0 1
     SPPLITTER_MODEL :int
 
     # ES_BASE_URL
@@ -42,38 +35,6 @@ class Settings(BaseSettings):
     MILVUS_PORT :int 
 
     RERANK_BASE_URL:str
-    
-    ONEAPI_BASE_URL:str
-    ONEAPI_API_KEY:str
-    ONEAPI_MODEL:str
-    ONEAPI_EMBEDDING_MODEL:str
-    
-    OPENAI_API_KEY:str
-    OPENAI_BASE_URL:str
-    OPENAI_MODEL:str
-    OPENAI_EMBEDDING_MODEL:str
-
-    ZHIPUAI_API_KEY:str
-    ZHIPUAI_MODEL: str
-
-    SPARKAI_APP_ID:str
-    SPARKAI_API_SECRET:str
-    SPARKAI_API_KEY:str
-    SPARKAI_DOMAIN:str
-    SPARKAI_BASE_URL:str
-
-    DOUBAOAI_API_KEY:str
-    DOUBAOAI_BASE_URL:str
-    DOUBAOAI_MODEL:str
-    DOUBAOAI_EMBEDDING_MODEL:str
-
-    OLLAMA_MODEL :str
-    OLLAMA_BASE_URL:str
-    
-    SILICONFLOW_API_KEY:str
-    SILICONFLOW_BASE_URL:str
-    SILICONFLOW_MODEL:str  # 或其他可用模型
-    SILICONFLOW_EMBEDDING_MODEL:str
      
     class Config:
         env_file = ".env"
