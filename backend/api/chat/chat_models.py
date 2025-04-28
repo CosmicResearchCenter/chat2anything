656c1f07d8ad2c75a182289b52979f2efd2f47a5
markyangkp <markyangkp@outlook.com>
2025-04-28 11:04:40 +0800

    新增聊天模型相关的 Pydantic 数据类，支持消息请求、响应及知识库操作

---
+++ backend/api/chat/chat_models.py
@@ -0,0 +1,120 @@
+from pydantic import BaseModel
+from typing import List, Optional,Dict,Any
+from core.rag.models.knolwedge_base import ResultByDoc
+from services.chat.chat_type import ChatMessageHistory
+
+class LLMConfig(BaseModel):
+    LLM_Provider:str
+    model:str
+
+# Chat message request
+class ChatMessageRequest(BaseModel):
+    streaming: bool
+    conversation_id: str
+    message: str
+    llm_info:LLMConfig
+
+
+# Chat message history response
+class ChatMessageHistoryResponse(BaseModel):
+    code: int
+    data: List[ChatMessageHistory]
+    message: str
+
+class ChatMessageResponse(BaseModel):
+    code: int
+    data: List[Any]
+    message: str
+class ChatConversationResponse(BaseModel):
+    code: int
+    data: List[Any]
+    message: str
+
+class ConversationCreateRequest(BaseModel):
+    knowledge_base_id: str
+
+class ConversationCreateResponse(BaseModel):
+    code: int
+    data: Optional[dict]
+    message: str
+
+# Knowledge base selection request
+class KnowledgeBaseSelectRequest(BaseModel):
+    conversation_id: str
+    knowledge_base_id: str
+
+# Knowledge base selection response
+class KnowledgeBaseSelectResponse(BaseModel):
+    code: int
+    data: List[Any]
+    message: str
+
+# Clear chat request
+class ChatClearRequest(BaseModel):
+    conversation_id: str
+
+# Clear chat response
+class ChatClearResponse(BaseModel):
+    code: int
+    data: List[Any]
+    message: str
+
+# Create knowledge base request
+class CreateKnowledgeBaseRequest(BaseModel):
+    name: str
+
+# Create knowledge base response
+class CreateKnowledgeBaseResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Knowledge base list response
+class KnowledgeBaseListResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Knowledge base details response
+class KnowledgeBaseDetailResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# Upload file request
+class UploadFileRequest(BaseModel):
+    file: str
+
+# Upload file response
+class UploadFileResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+# File index status response
+class FileIndexStatusResponse(BaseModel):
+    code: int
+    data: List[Optional[dict]]
+    message: str
+
+class DeleteConversationResponse(BaseModel):
+    code: int
+    data: Optional[dict]
+    message: str
+
+class ReNameRequest(BaseModel):
+    conversation_id: str
+    new_name: str
+
+class ReNameResponse(BaseModel):
+    code: int
+    data: Optional[dict]
+    message: str
+
+class ConversationTitleCreateRequest(BaseModel):
+    conversation_id: str
+
+class ConversationTitleCreateResponse(BaseModel):
+    code: int
+    data: Optional[Any]
+    message: str
